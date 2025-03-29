78c5096fe349e8531f749c46bee0143b35f481dd
sammmmmm099 <srnvsxig@gmail.com>
2025-03-29 17:27:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "979826"))
+API_HASH = environ.get("API_HASH", "238183386c30590d073b457166ba260d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7511409204:AAGU9jBa0zIwtQwZBbQnTjtoI5BDWY1AzHM")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002636069197"))
+ADMINS = int(environ.get("ADMINS", "1074804932"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+DB_NAME = environ.get("DB_NAME", "JRA")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
