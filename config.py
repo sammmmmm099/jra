d365a64f610afff222d1489ed6203b01590390ad
sammmmmm099 <srnvsxig@gmail.com>
2025-03-29 18:00:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002636069197"))
 ADMINS = int(environ.get("ADMINS", "1074804932"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "mongodb+srv://sainallamilli17:Animes3u@jra.bhuokyq.mongodb.net/?retryWrites=true&w=majority&appName=JRA") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "JRA")
 
 # If this is True Then Bot Accept New Join Request 
